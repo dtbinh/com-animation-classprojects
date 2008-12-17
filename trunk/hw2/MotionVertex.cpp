6bf6dc60e98555d70c4c7332611650980d8eec33
grahamhuang <grahamhuang@73147322-a748-11dd-a8c6-677c70d10fe4>
2008-12-17 14:42:08 +0000

    Add traversal of motion graph, but undone.

--- trunk/hw2/MotionVertex.cpp
+++ trunk/hw2/MotionVertex.cpp
@@ -3,4 +3,17 @@
 MotionVertex::MotionVertex()
 {
 	m_InSCC = false;
+	m_NumSCCAdj = 0;
+}
+
+void MotionVertex::computeNumSCCAdj()
+{
+	int i;
+	int size = m_AdjVertices.size();
+
+	for (i = 0; i < size; i++)
+	{
+		if (m_AdjVertices[i]->m_InSCC)
+			m_NumSCCAdj++;
+	}
 }
