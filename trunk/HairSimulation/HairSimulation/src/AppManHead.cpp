febd01e6dc7f0d30b2f7c6d56e7a74a8b1626733
grahamhuang <grahamhuang@73147322-a748-11dd-a8c6-677c70d10fe4>
2009-01-09 04:53:49 +0000

    Add the classes "AppManHead" for man head object in the scene.

---
+++ trunk/HairSimulation/HairSimulation/src/AppManHead.cpp
@@ -0,0 +1,28 @@
+#include "AppManHead.h"
+#include "World.h"
+
+//-------------------------------------------------------------------------
+ManHead::ManHead(const String& name) : ApplicationObject(name)
+{
+    setUp(name);
+}
+//-------------------------------------------------------------------------
+ManHead::~ManHead()
+{
+}
+//-------------------------------------------------------------------------
+void ManHead::setUp(const String& name)
+{
+    // Create visual presence
+    SceneManager* sm = World::getSingleton().getSceneManager();
+    mEntity = sm->createEntity(name, "facial.mesh");
+    mSceneNode = sm->getRootSceneNode()->createChildSceneNode(name);
+    mSceneNode->attachObject(mEntity);
+	
+	Real scale = Real(0.5f);
+	mSceneNode->scale(scale, scale, scale);
+
+    // Add reverse reference
+    mEntity->setUserObject(this);
+
+}
