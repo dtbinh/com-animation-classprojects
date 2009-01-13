5932e47c9b22345a45cc7f31d7af846710ed9d0c
grahamhuang <grahamhuang@73147322-a748-11dd-a8c6-677c70d10fe4>
2009-01-13 15:20:40 +0000

    Add "getMeshInformation" method in class "ApplicationObject".

--- trunk/HairSimulation/HairSimulation/include/ApplicationObject.h
+++ trunk/HairSimulation/HairSimulation/include/ApplicationObject.h
@@ -12,25 +12,47 @@ protected:
 	SceneNode* mSceneNode;
 	Entity* mEntity;
 
+	//	Mesh information
+	size_t mVertexCount;
+	size_t mIndexCount;
+	Vector3* mVertices;
+	unsigned long* mIndices;
+
 	//		Set up method, must override
 	virtual void setUp(const String& name) = 0;
 
 public:
-	ApplicationObject(const String& name);
-	virtual ~ApplicationObject();
+		ApplicationObject(const String& name);
+		virtual ~ApplicationObject();
 
-	/** Sets the position of this object. */
-    virtual void setPosition(const Vector3& vec);
-    /** Sets the position of this object. */
-    virtual void setPosition(Real x, Real y, Real z);
-	/** Gets the current position of this object. */
-    virtual const Vector3& getPosition(void);
+		/** Sets the position of this object. */
+		virtual void setPosition(const Vector3& vec);
+		/** Sets the position of this object. */
+		virtual void setPosition(Real x, Real y, Real z);
+		/** Gets the current position of this object. */
+		virtual const Vector3& getPosition(void);
 
-	/** Gets the SceneNode which is being used to represent this object's position in 
+		/** Gets the SceneNode which is being used to represent this object's position in 
             the OGRE world. */
         SceneNode* getSceneNode(void);
         /** Gets the Entity which is being used to represent this object in the OGRE world. */
         Entity* getEntity(void);
+
+		//------------------------------------------------------------------------------
+		size_t getVertexCount(void);
+
+		//------------------------------------------------------------------------------
+		size_t getIndexCount(void);
+
+		//------------------------------------------------------------------------------
+		Vector3* getVertices(void);
+
+		//------------------------------------------------------------------------------
+		unsigned long* getIndices(void);
+
+		/** Get mesh information */
+		void generateMeshInfo(void);
+			
 };
 
 #endif
--- trunk/HairSimulation/HairSimulation/src/ApplicationObject.cpp
+++ trunk/HairSimulation/HairSimulation/src/ApplicationObject.cpp
@@ -1,11 +1,16 @@
 #include "ApplicationObject.h"
 #include "World.h"
+#include "AppUtilities.h"
 
 //-------------------------------------------------------------------------
 ApplicationObject::ApplicationObject(const String& name)
 {
 	mSceneNode = 0;
 	mEntity = 0;
+	mIndexCount = 0;
+	mIndices = 0;
+	mVertexCount = 0;
+	mVertices = 0;
 }
 //-------------------------------------------------------------------------
 ApplicationObject::~ApplicationObject()
@@ -41,4 +46,41 @@ SceneNode* ApplicationObject::getSceneNode(void)
 Entity* ApplicationObject::getEntity(void)
 {
     return mEntity;
+}
+
+//-------------------------------------------------------------------------
+size_t ApplicationObject::getVertexCount(void)
+{
+	return mVertexCount;
+}
+
+//-------------------------------------------------------------------------
+size_t ApplicationObject::getIndexCount(void)
+{
+	return mIndexCount;
+}
+
+//-------------------------------------------------------------------------
+Vector3* ApplicationObject::getVertices(void)
+{
+	return mVertices;
+}
+
+//-------------------------------------------------------------------------
+unsigned long* ApplicationObject::getIndices(void)
+{
+	return mIndices;
+}
+
+//-------------------------------------------------------------------------
+void ApplicationObject::generateMeshInfo(void)
+{
+	if ((mSceneNode == NULL) || (mEntity == NULL))
+	{
+		std::cout << "Error in ApplicationObject::generateMeshInfo()" << std::endl;
+		return;
+	}
+
+	Utilities::getMeshInformation(mEntity->getMesh(), mVertexCount, mVertices, mIndexCount, mIndices,
+		mSceneNode->getWorldPosition(), mSceneNode->getWorldOrientation(), mSceneNode->getScale());
 }
